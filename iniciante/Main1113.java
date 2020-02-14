d9b1abff1c837da48c67bbcd288b278806859ad2
John William Vicente <58832150+JohnWill14@users.noreply.github.com>
2020-02-14 10:11:30 -0300

    Main1113

---
+++ iniciante/Main1113.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.PrintStream;
+public class Main1113{
+	private static Scanner sc;
+	private static PrintStream out;
+	static{
+		sc=new Scanner(System.in);
+		out=System.out;
+	}
+	public static void main(String args[]){
+		repeticao:while(true){
+			int a=sc.nextInt(),b=sc.nextInt();
+			if(a==b)break repeticao;
+			else if(a<b)out.println("crescente");
+			else out.println("decrescente");
+		}
+	}
+
+}
