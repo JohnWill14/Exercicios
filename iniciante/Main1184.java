cb59ed35d5499fc98b10268e23f4d768ad921fe5
John William Vicente <58832150+JohnWill14@users.noreply.github.com>
2020-03-02 08:24:09 -0300

    Main1184

---
+++ iniciante/Main1184.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.io.PrintStream;
+public class Main1184{
+	private static Scanner sc;
+	private static PrintStream ps;
+	static{
+		sc=new Scanner(System.in);
+		ps=System.out;
+	}
+	public static void main(String... args){
+		double[][] matriz=new double[12][12];
+
+		String c =sc.next();
+		for(int i=0;i<12;i++)
+			for(int j=0;j<12;j++)
+				matriz[i][j]=sc.nextDouble();
+		double res=0;
+		int count=0;
+		for(int i=0;i<12;i++){
+			for(int j=0;j<12;j++)
+				if(j<i){
+					count+=1;
+					res+=matriz[i][j];
+				}
+		}
+		if(c.equals("S")){
+			res=res;
+		}
+		else{
+
+			res/=count;
+		}
+		ps.println(String.format("%.1f",res));
+		
+	}
+}
